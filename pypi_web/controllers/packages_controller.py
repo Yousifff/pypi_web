d9d0f7301733c2b5ba412a74523702e7f9c4b667
yousif <yousifsayer@gmail.com>
2024-04-27 21:47:00 +0300

    First Commitaa

--- pypi_web/controllers/packages_controller.py
+++ pypi_web/controllers/packages_controller.py
@@ -1,6 +1,6 @@
 from pyramid.view import view_config
-from pyramid.request import  Request
-import pyramid.httpexceptions as x
+from pyramid.request import Request
+from pyramid.httpexceptions import HTTPNotFound
 
 #https://pypi.org/project/fastapi/
 
@@ -9,10 +9,8 @@ import pyramid.httpexceptions as x
 @view_config(route_name='package_details',renderer='pypi_web:templates/packages/details.pt')
 @view_config(route_name='package_details/',renderer='pypi_web:templates/packages/details.pt')
 def details(request: Request):
-    package_name = request.matchdict['package_name']
-    if not package_name:
-        raise x.HTTPNotFound()
-    return {
-        'package_name': package_name
-    }
+    package_name = request.matchdict.get('package_name')
+    if not package_name or package_name == 'gone':
+        raise HTTPNotFound()
+    return {'message': 'Hello World'}
 
